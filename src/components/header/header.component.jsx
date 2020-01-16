4130f0b720f5499f19a2f8a6e01bc0aa126f6a34
Ryan Akins <ryan.akins@wilksbrothers.com>
2020-01-15 20:08:47 -0600

    adding redux and user reducer, user actions to header and app components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils'
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
@@ -7,7 +8,7 @@ import './header.styles.scss';
 const Header = ({ currentUser }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
-            <Logo className='logo' />
+            <Logo className='logo' />  CRWN Clothing
         </Link>
         <div className='options'>
             <Link className='option' to="/shop">
@@ -27,6 +28,10 @@ const Header = ({ currentUser }) => (
             
         </div>
     </div>
-)
+);
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps )(Header);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { signInWithGoogle, auth } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -14,17 +14,24 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
-        event.preventDeafult();
+    handleSubmit = async event => {
+        event.preventDefault();
 
-        this.setState({ email: '', password: ''});
-    }
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            //this.setState({ email: '', password: ''});
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     handleChange = event => {
         const { value, name } = event.target;
 
         this.setState({ [name]: value });
-    }
+    };
 
     render() {
         return (
@@ -48,7 +55,7 @@ class SignIn extends React.Component {
                         label='password'
                         required />
                     <div className='buttons'>
-                        <CustomButton type="submit">Sign In</CustomButton>
+                        <CustomButton type='submit'>Sign In</CustomButton>
                         <CustomButton onClick={signInWithGoogle} isGoogleSignIn> 
                             {' '}
                             Sign in with Google{' '}
