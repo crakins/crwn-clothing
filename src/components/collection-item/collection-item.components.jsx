4911dc1f3b6350c22ef4277248cfddb857ce3eaf
Ryan Akins <ryan.akins@wilksbrothers.com>
2020-01-17 07:53:40 -0600

    add to cart button on collection-item and styles

--- src/components/collection-item/collection-item.components.jsx
+++ src/components/collection-item/collection-item.components.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import CustomButton from '../custom-button/custom-button.component';
+
 import './collection-item.styles.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
@@ -11,8 +14,9 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
          />
         <div className='collection-footer'>
             <span className='name'>{name}</span>
-            <span className='price'>{price} </span>
+            <span className='price'>${price} </span>
         </div>
+        <CustomButton inverted> Add to Cart </CustomButton>
     </div>
 )
 
