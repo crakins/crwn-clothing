9385d53c39fafa1fdbefad96126c4841746f5c71
Ryan Akins <ryan.akins@wilksbrothers.com>
2020-01-20 17:19:47 -0600

    added reselect library, replaced selectors with memoized selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,28 @@
+import { createSelector } from 'reselect';
+
+// MEMOIZED or cached selectors 
+
+// 2 types, input and output - both are required
+
+// input selector
+// a function that takes the whole state that only returns one piece of state
+
+const selectCart = state => state.cart;
+
+//output selector
+
+export const selectCartsItems = createSelector(
+    [selectCart],
+    (cart) => cart.cartItems
+);
+
+// example of a selector for the cart item count in MapToProps in Cart-icon component
+
+export const selectCartItemsCount = createSelector(
+    [selectCartsItems],
+    cartItems =>
+        cartItems.reduce(
+            (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
+            0
+        )
+);
