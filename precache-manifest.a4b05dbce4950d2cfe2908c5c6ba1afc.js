202ebdfcffff4958cc4e9d677e63b0cde9f3072b
Ryan Akins <ryan.akins@wilksbrothers.com>
2020-01-20 20:23:09 -0600

    Updates

---
+++ precache-manifest.a4b05dbce4950d2cfe2908c5c6ba1afc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c832f3bbc451c5ceba0051d5d4b39029",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "064328045c1c4c3b8d62",
+    "url": "/crwn-clothing/static/css/main.a7781896.chunk.css"
+  },
+  {
+    "revision": "1b3a2af622b423b649c5",
+    "url": "/crwn-clothing/static/js/2.4fc7924a.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/crwn-clothing/static/js/2.4fc7924a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "064328045c1c4c3b8d62",
+    "url": "/crwn-clothing/static/js/main.594cb989.chunk.js"
+  },
+  {
+    "revision": "8e1f379f0858b5b017bc",
+    "url": "/crwn-clothing/static/js/runtime-main.01128f07.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
--- src/redux/user/user.types.js
+++
@@ -1,5 +0,0 @@
-export const UserActionTypes = {
-    SET_CURRENT_USER: 'SET_CURRENT_USER',
-    GET_USERS: 'GET_USERS'
-
-};
